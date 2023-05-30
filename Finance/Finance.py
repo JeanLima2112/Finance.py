5fea7dee8447b2255397be3f79624cd4c0e22110
Jean Lima <132618223+JeanLima2112@users.noreply.github.com>
2023-05-29 21:24:03 -0300

    Update Finance.py

--- Finance/Finance.py
+++ Finance/Finance.py
@@ -1,7 +1,23 @@
-from kivy.app import App
-from kivy.uix.button import Button
+from tkinter import *
 
-class Test(App):
-    def build(self):
-        return Button(text= 'Primeiro Botão em Kivy')
-Test().run()
+def oi():
+    oi = ("kkkkkkk, muito gay")
+    resp["text"] = oi
+
+janela = Tk()
+janela.title("TESTE LGBT")
+janela.geometry("300x200")
+janela.config(bg='#DA70D6 ')
+textoincial = Label(janela, text=("Clica no botão se você é gay"), bg="pink", fg="black")
+textoincial.grid (column=0, row=0, padx=10, pady=10)
+
+texto2 = Label(janela, text=("Esse botão aqui!"), bg="pink", fg="black")
+texto2.grid (column=0,row=1, padx=10, pady=10)
+
+botao = Button(janela, text="Vai clicar mesmo?", bg="pink", fg="black",command=oi)
+botao.grid(column=0,row=2, padx=10, pady=10)
+
+resp= Label(janela, text="",bg="pink", fg="black")
+resp.grid (column=0,row=3, padx=10, pady=10)
+
+janela.mainloop()
